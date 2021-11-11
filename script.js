2fadcc456339b819dc8c0633af1e95c8845a0e85
George-Anto <george3osfp@gmail.com>
2021-11-11 15:10:57 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,79 @@
+"use strict";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+const modal = document.querySelector(".modal-window");
+const overlay = document.querySelector(".overlay");
+
+const btnCloseModal = document.querySelector(".close-modal");
+const btnShowModal = document.querySelector(".show-modal");
+
+const motivationalQuote = document.querySelector(".quote");
+const motivationalQuoteAuthor = document.querySelector(".author");
+
+const btnDownloadCV = document.querySelector(".downloadCV");
+
+const closeModal = function () {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+};
+
+const showModal = function () {
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
+};
+
+const fetchQuotes = async function () {
+	try {
+		//Does NOT work
+		// const responce = await fetch("resources/testQuote.json");
+		// const quotes = await JSON.parse(JSON.stringify(responce));
+		// console.log(quotes);
+
+		return fetch("resources/data/quotes.json")
+			.then(responce => responce.json())
+			.then(data => {
+				return data;
+			});
+	} catch (error) {
+		console.error(`Custom Error1: ${error}`);
+	}
+};
+
+const insertQuoteInDOM = async function () {
+	try {
+		const quoteObj = await fetchQuotes();
+
+		const randomQuoteNumber = Math.floor(Math.random() * 102) + 1;
+
+		motivationalQuote.innerHTML = "";
+		motivationalQuote.innerHTML = `${quoteObj.quotes[randomQuoteNumber].quote}`;
+
+		motivationalQuoteAuthor.innerHTML = "";
+		motivationalQuoteAuthor.innerHTML = `${quoteObj.quotes[randomQuoteNumber].author}`;
+	} catch (error) {
+		console.error(`Custom Error2: ${error}`);
+	}
+};
+
+const pdfCVNotReady = function () {
+	alert(
+		`Το ηλεκτρονικό αντίτυπο του βιογραφικού που θα είναι διαθέσιμο για λήψη, δεν έχει ολοκληρωθεί ακόμα.`
+	);
+};
+
+btnShowModal.addEventListener("click", showModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (keyEventObj) {
+	if (keyEventObj.key === "Escape" && !modal.classList.contains("hidden")) {
+		closeModal();
+	}
+});
+
+btnDownloadCV.addEventListener("click", pdfCVNotReady);
+
+insertQuoteInDOM();
